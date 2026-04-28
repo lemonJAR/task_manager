c1da59ff58b554e644e50cb92c14965c7227b1a9
John Anthony Romeo <jaromeo65@gmail.com>
2026-04-28 11:02:03 +0800

    feat: add GitHub Actions CI/CD pipeline

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,39 +5,35 @@ plugins {
     id("dev.flutter.flutter-gradle-plugin")
 }
 
+def keystoreProperties = new Properties()
+def keystorePropertiesFile = rootProject.file('key.properties')
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(
+        new FileInputStream(keystorePropertiesFile))
+}
+ 
 android {
-    namespace = "ph.edu.usjr.first_app.task_manager"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+    ...
+    signingConfigs {
+        release {
+            keyAlias keystoreProperties['keyAlias'] \
+                ?: System.getenv('KEY_ALIAS')
+            keyPassword keystoreProperties['keyPassword'] \
+                ?: System.getenv('KEY_PASSWORD')
+            storeFile keystoreProperties['storeFile'] ?
+                file(keystoreProperties['storeFile']) :
+                file(System.getenv('KEYSTORE_PATH'))
+            storePassword keystoreProperties['storePassword'] \
+                ?: System.getenv('STORE_PASSWORD')
+        }
     }
-
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
-    }
-
-    defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "ph.edu.usjr.first_app.task_manager"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
-    }
-
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig signingConfigs.release
         }
     }
 }
+
 
 flutter {
     source = "../.."
